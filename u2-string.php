f3666622be3326a72080065e49edc8e08af63e3f
carekb684 <carekb684@E29645.edu.linkoping.se>
2014-09-01 10:23:28 +0200

    fem(klar)

--- u2-string.php
+++ u2-string.php
@@ -1,11 +1,16 @@
 <?php
-
+header('Content-Type: text/html; charset=utf-8');
     $namn1="Carl";
     $namn2="Jennifer Lawrence";
     
     echo $namn1 . " tar fina bilder på " . $namn2;
     
-    $jenni = substr($namn2, 0, 9);
+    $jenni = substr($namn2, 0, 8);
     
+    if ($jenni=="Jennifer") {
+   
     echo "<br>" . $jenni;
+    }else{
+        echo"Okaj";
+    }
 ?>
